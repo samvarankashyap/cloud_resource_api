f18ff9322e15ba6458d3b44f376e978ad281747f
samvarankashyap <samvaran2best@gmail.com>
2016-07-24 16:02:06 -0400

    added list_creds api

--- restycloud/urls.py
+++ restycloud/urls.py
@@ -3,4 +3,5 @@ from restycloud import views
 
 urlpatterns = [
     url(r'hello$', views.hello),
+    url(r'list_creds$', views.list_creds),
 ]
--- restycloud/views.py
+++ restycloud/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 import pdb
+import os
 class JSONResponse(HttpResponse):
     """
     An HttpResponse that renders its content into JSON.
@@ -17,9 +19,21 @@ class JSONResponse(HttpResponse):
 @csrf_exempt
 def hello(request):
     """
-    List all code snippets, or create a new snippet.
+    responds hello on get.
     """
     if request.method == 'GET':
         snippets = {"hello": "this is GET"}
         return JSONResponse(snippets)
 
+@csrf_exempt
+def list_creds(request):
+    """
+    responds with list of credentials available
+    """
+    if request.method == 'GET':
+        directory_list = os.listdir(settings.CREDS_DIR)
+        creds = {}
+        for provider in directory_list:
+            creds[provider] = os.listdir(settings.CREDS_DIR+"/"+provider)
+        creds['directories'] =  directory_list
+        return JSONResponse(creds)
