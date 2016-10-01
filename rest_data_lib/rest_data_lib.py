5521f285a883c6d2c16a7d3309c4cfe718949395
Samvaran Rallabandi <srallaba@redhat.com>
2016-10-01 19:10:44 -0400

    added skeleton for rest_data_lib

--- rest_data_lib/rest_data_lib.py
+++ rest_data_lib/rest_data_lib.py
@@ -12,3 +12,27 @@ def get_os_object_data():
 
 def get_os_volume_data():
     pass
+
+def get_aws_ec2_data():
+    pass
+
+def get_aws_s3_data():
+    pass
+
+def get_aws_ec2_key_data():
+    pass
+
+def get_aws_cfn_data():
+    pass
+
+def get_gcloud_gce_data():
+    pass
+
+def get_duffy_data():
+    pass
+
+def get_libvirt_data():
+    pass
+
+def get_rax_server_data():
+    pass
